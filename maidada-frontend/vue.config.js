2bf58969032b17f6142c2981a27b0a2ef5a852c0
teng <teng2002823@outlook.com>
2024-06-03 16:10:29 +0800

    解决本地屏幕尺寸更改BUG

--- maidada-frontend/vue.config.js
+++ maidada-frontend/vue.config.js
@@ -2,4 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  devServer: {
+    client: {
+      overlay: false,
+    },
+  },
 });
