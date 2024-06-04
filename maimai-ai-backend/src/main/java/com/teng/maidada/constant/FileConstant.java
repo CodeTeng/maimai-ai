fb48dd7fffe80089d9b3614cd46c08ea76d44dd4
teng <teng2002823@outlook.com>
2024-06-04 12:33:31 +0800

    增加 TODO

--- maimai-ai-backend/src/main/java/com/teng/maidada/constant/FileConstant.java
+++ maimai-ai-backend/src/main/java/com/teng/maidada/constant/FileConstant.java
@@ -4,11 +4,10 @@ package com.teng.maidada.constant;
  * 文件常量
  *
  * @author 程序员麦麦
- *
  */
 public interface FileConstant {
     /**
-     * COS 访问地址
+     * TODO COS 访问地址
      */
     String COS_HOST = "https://muziteng-1310538376.cos.ap-beijing.myqcloud.com";
 }
